64f574848372273f75ddb8cec1a56bb16b8454a5
FabianFdz <fa.fdz@me.com>
2014-10-29 02:57:28 -0600

    Trabajo por hacer al final de documento prueba.rb

--- prueba.rb
+++ prueba.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
+require 'haml' #Se tuvo que instalar
 
 get '/' do
 	"Hello World!"
 end
 
-get '/hello/:name' do
+get '/hello/:name' do #recibe usuario-contrasenna. Ejm: fdz-123
   # matches "GET /hello/foo" and "GET /hello/bar"
   # params[:name] is 'foo' or 'bar'
   "Hello #{params[:name]}!"
 end
+
+# Investigar manejo de archivos .txt en Ruby
+# Implementar para registrar (/usuario-contrasenna)
+# => Debe verificar que el usuario no exista
+# => Insertar en *.txt
+# Implementar log in de usuarios
+# => Verificar que tenga la contrasenna correcta mientras verifica que existe el usuario
+# Preguntas:
+# Para asociar preguntas con el usuario (/usuario-pregunta)
+# Cada pregunta tiene un *.txt
+# => La primer linea va a ser la pregunta y va a estar con el formato usuario;,pregunta;,etiquetas
+# => El resto de lineas son respuestas y tienen el formato usuario;,respuesta
+# Respuestas:
+# Para que una pregunta reciba una respuesta se recibe lo siguiente usuario_pregunta-pregunta-usuario_responde-respuesta
+# Cada respuesta se almacena al final del documento
+# Cuando se va a devolver de la mas reciente a la mas antigua se saca de abajo para arriba (Se lee normal pero se le da vuelta a la lista)
